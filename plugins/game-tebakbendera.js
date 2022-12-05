85ef2fa75a6b6734d25ef57b9d938c7d6d29a6c8
audikirito <93035582+audikirito@users.noreply.github.com>
2022-12-06 00:12:05 +0700

    Update game-tebakbendera.js

--- plugins/game-tebakbendera.js
+++ plugins/game-tebakbendera.js
@@ -1,33 +1,40 @@
 import fetch from 'node-fetch'
-let timeout = 120000
-let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
-  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-  let id = m.chat
-  if (id in conn.tebakbendera) {
-    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
-    throw false
-  }
-  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera2.json')).json()
+let timeout = 60000
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.sendButton(m.chat, '*_Masih Ada Soal Belum Terjawab Di Chat Ini_*', author, null, buttons, conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera2.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-  let caption = `
-  Bendera apakah ini?
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tebe untuk bantuan
-Bonus: ${poin} XP
+    let caption = `*${command.toUpperCase()} Games 🎮*
+
+*⏱️ Timeout ${(timeout / 1000).toFixed(2)} Detik*
+*🔍 Ketik* ${usedPrefix}hben Untuk Bantuan
+*🎁 Prize* ${poin} XP
     `.trim()
-  conn.tebakbendera[id] = [
-    await conn.sendButtonImg(m.chat, json.img, caption, wm3, 'Bantuan', '.tebe', m)
-    ,
-    json, poin,
-    setTimeout(async () => {
-      if (conn.tebakbendera[id]) await conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, '', 'Tebak Bendera', '.tebakbendera', conn.tebakbendera[id][0])
-      delete conn.tebakbendera[id]
-    }, timeout)
-  ]
+    conn.tebakbendera[id] = [
+        await conn.sendButton(m.chat, caption, author, json.img, buttons, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.sendButton(m.chat, `*_Waktu Habis_*\nJawabannya Adalah *${json.name}*`, author, null, [
+                ['𝗧𝗘𝗕𝗔𝗞-𝗕𝗘𝗡𝗗𝗘𝗥𝗔', '/tebakbendera']
+            ], conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
 }
 handler.help = ['tebakbendera']
 handler.tags = ['game']
 handler.command = /^tebakbendera/i
-
+handler.register = true
+handler.limit = 1
 export default handler
+
+const buttons = [
+    ['𝗕𝗔𝗡𝗧𝗨𝗔𝗡', '/hben'],
+    ['𝗡𝗬𝗘𝗥𝗔𝗛', 'menyerah']
+]
