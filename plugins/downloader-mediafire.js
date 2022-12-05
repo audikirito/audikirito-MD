12aed8a7aa672af04eefafa9019171b3357df049
audikirito <93035582+audikirito@users.noreply.github.com>
2022-12-06 00:05:35 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,6 +1,9 @@
 import { mediafiredl } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+try {
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
     let caption = `
@@ -11,9 +14,20 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 `.trim()
     m.reply(caption)
     await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+    } catch {
+    let res = await fetch(`https://api.lolhuman.xyz/api/mediafire?apikey=${lolkey}&url=${args[0]}`)
+    let x = await res.json()
+    let caption = `*${htki} mediafire ${htka}*
+*💌 title:* ${x.result.filename}
+*🗂️ filetype:* ${x.result.filetype}
+*📊 filesize:* ${x.result.filesize}
+*📨 uploaded:* ${x.result.uploaded}
+    `
+    conn.sendButtonImg(m.chat, thumbs, caption, author, 'G E T', '.get ' + x.result.link, fakes, adReply)
+    }
 }
 handler.help = ['mediafire'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+handler.tags = ['downloader', 'limitmenu']
 handler.command = /^(mediafire|mf)$/i
 
 handler.limit = true
