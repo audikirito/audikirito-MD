749315c6e7adf74699dfe484e00c5a9d9d946e3e
audikirito <93035582+audikirito@users.noreply.github.com>
2022-12-08 13:54:16 +0700

    Update anime-jahy.js

--- plugins/anime-jahy.js
+++ plugins/anime-jahy.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.xteam.xyz/randomimage/jahy?APIKEY=Apikeymu'
+	let url = 'https://api.xteam.xyz/randomimage/jahy?APIKEY=ebb6251cc00f9c63'
 	conn.sendButton(m.chat, 'Wangy wangy (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(jahy)$/i
-handler.tags = ['anime', 'premium', 'nsfw']
+handler.tags = ['anime', 'limit', 'nsfw']
 handler.help = ['jahy']
-handler.premium = true
+handler.premium = false
+handler.register = true
 
 export default handler
