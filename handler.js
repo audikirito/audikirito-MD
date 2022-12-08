2da8108aa5b15569d3bec22a24487890beffd3e7
audikirito <93035582+audikirito@users.noreply.github.com>
2022-12-08 12:42:14 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -7,6 +7,7 @@ import chalk from 'chalk'
 import fs from 'fs'
 import fetch from 'node-fetch'
 import moment from 'moment-timezone'
+import { weeaboo } from './lib/weeaboo.js'
 
 /**
  * @type {import('@adiwajshing/baileys')}
