95de0806ec39b52f85922063c5647916346daa95
audikirito <93035582+audikirito@users.noreply.github.com>
2022-12-24 05:53:00 +0700

    Update downloader-tiktokmp3.js

--- plugins/downloader-tiktokmp3.js
+++ plugins/downloader-tiktokmp3.js
@@ -1,25 +1,52 @@
-import axios from 'axios'
 import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok2', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    
-conn.sendFile(m.chat, res.audio, 'error.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: false, seconds: 0,contextInfo: {
-         externalAdReply: { showAdAttribution: true,
- mediaUrl: snh,
-    mediaType: 2, 
-    description: snh,
-    title: "Nɪʜ...",
-    body: wm,
-    thumbnail: await (await fetch('https://telegra.ph/file/cce9ab4551f7150f1970d.jpg')).buffer(),
-    sourceUrl: snh}}})
+import axios from 'axios'
+import fs from 'fs'
+
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let chat = global.db.data.chats[m.chat]
+  m.reply(wait)
+  await conn.reply(m.chat, `Downloading media from Tiktok`, 0, {
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        mediaUrl: '',
+        mediaType: 2,
+        description: sgc,
+        title: global.wm,
+        body: 'Nih Kak', //`${fileSizeH}`,
+        sourceUrl: snh,
+        thumbnail: fs.readFileSync('./thumbnail.jpg')
+      }
+    }
+  })
+  let url = `https://api.lolhuman.xyz/api/tiktokwm?apikey=1a5d403c0f75c316c9920b35&url=${args[0]}`
+  let txt = `🚀 *Link:* ${await (await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}`
+  await conn.sendFile(m.chat, url, 'tiktokaudio.mp3', `
+┏┉━━━━━━━━━━━❏
+┆ *TIKTOK MP3*
+├┈┈┈┈┈┈┈┈┈┈┈
+┆• *Judul:* 
+│• *Type:* MP3
+┆• *📥 Ukuran File:* 
+└❏
+`.trim(), m, null, {
+    document: { url }, mimetype: 'audio/mpeg', fileName: 'tiktok.mp3', conntextInfo: {
+      externalAdReply: {
+        title: '▶︎ ━━━━━━━•────────────────── ',
+        body: 'Now Playing...',
+        description: 'Now Playing...',
+        mediaType: 2,
+        thumbnail: await (await fetch('https://telegra.ph/file/cce9ab4551f7150f1970d.jpg')).buffer(),
+        mediaUrl: `https://telegra.ph/file/cce9ab4551f7150f1970d.jpg`
+      }
+    }
+  })
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktokmp3|ttdlmp3|ttmp3|tiktokdlmp3|gettt)$/i
+handler.command = /^(tt|tiktok)(a(udio)?|mp3|sound)(dl)?(download(er)?)?$/i
+
+handler.limit = true
 
 export default handler
+
